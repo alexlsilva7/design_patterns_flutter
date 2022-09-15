fa2b5011785918cdc73d0b4c53a1fe7ca0bdf670
Alex Lopes <alex.lsilva154@gmail.com>
2022-09-15 19:55:51 -0300

    model contact usada em adapter

---
+++ lib/adapter/contact.dart
@@ -0,0 +1,11 @@
+class Contact {
+  final String fullName;
+  final String email;
+  final bool favorite;
+
+  Contact({
+    required this.fullName,
+    required this.email,
+    required this.favorite,
+  });
+}
