acdd5eab796a5bc37ead638c0cff57c76195ef9b
Alex Lopes <alex.lsilva154@gmail.com>
2022-09-16 19:46:28 -0300

    factory fromXml em contact

--- lib/adapter/models/contact.dart
+++ lib/adapter/models/contact.dart
@@ -1,3 +1,5 @@
+import 'package:xml/xml.dart';
+
 class Contact {
   final String fullName;
   final String email;
@@ -14,6 +16,13 @@ class Contact {
       fullName: json['full_name'],
       email: json['email'],
       favorite: json['favorite'],
+    );
+  }
+  factory Contact.fromXML(XmlElement xml) {
+    return Contact(
+      fullName: xml.findElements('full_name').first.text,
+      email: xml.findElements('email').first.text,
+      favorite: xml.findElements('favorite').first.text == 'true',
     );
   }
 }
